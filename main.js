e473da9c5d76217e2e3cb3a480697be82353876b
Faiaz <fayajassassin@gmail.com>
2026-08-27 23:22:31 +0600

    Initial typing test project

---
+++ main.js
@@ -0,0 +1,10 @@
+let timer = 60;
+const quotes = ["Hi"];
+let hasStarted = false;
+
+const quoteDisplay = document.getElementById("quote-display");
+const input = document.getElementById("typing-input"); 
+quoteDisplay.textContent = "Hello world!";
+quoteDisplay.innerHTML = "<span>H</span>";
+input.value;
+input.disabled = false;
